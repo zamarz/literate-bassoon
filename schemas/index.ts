2fe7abb043da1a0c58bf3881478bfc84323aad13
zamarz <zachmarzouk@gmail.com>
2024-01-18 14:26:25 +0000

    Create review schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,7 @@
 import { account } from "./account";
 import { booking } from "./booking";
 import { hotelRoom } from "./hotelRoom";
+import { review } from "./review";
 import { user } from "./user";
 
-export const schemaTypes = [user, account, booking, hotelRoom];
+export const schemaTypes = [user, account, booking, hotelRoom, review];
