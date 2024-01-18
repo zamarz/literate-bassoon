cedc477c99ad40d55069288896c326eee29f852f
zamarz <zachmarzouk@gmail.com>
2024-01-18 15:37:00 +0000

    Create verification schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,5 +3,13 @@ import { booking } from "./booking";
 import { hotelRoom } from "./hotelRoom";
 import { review } from "./review";
 import { user } from "./user";
+import { verificationToken } from "./verification";
 
-export const schemaTypes = [user, account, booking, hotelRoom, review];
+export const schemaTypes = [
+  user,
+  account,
+  booking,
+  hotelRoom,
+  review,
+  verificationToken,
+];
