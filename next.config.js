5ba8d0b2663a7ea52282941d88b27b3c3c795abd
zamarz <zachmarzouk@gmail.com>
2024-01-24 10:34:46 +0000

    Make featured room functional

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "images.unsplash.com",
+      "plus.unsplash.com",
+    ],
   },
 };
 
--- src/components/FeaturedRoom/FeaturedRoom.tsx
+++ src/components/FeaturedRoom/FeaturedRoom.tsx
@@ -1,5 +1,6 @@
 import { Room } from "@/app/models/room";
 import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
 type Props = {
@@ -19,6 +20,46 @@ export const FeaturedRoom: FC<Props> = (props) => {
             height={300}
             className="img scale-animation"
           />
+        </div>
+        <div className="grid grid-cols-2 gap-8 h-48">
+          {featuredRoom.images.splice(1, 2).map((image) => (
+            <div key={image._key} className="rounded-2xl overflow-hidden">
+              <Image
+                src={image.url}
+                alt={image._key}
+                width={300}
+                height={300}
+                className="img scale-animation"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="md:py-10 md:w-1/2 text-left ">
+        <h3 className="font-heading mb-12">Featured Room</h3>
+        <p className="font-normal max-w-md">{featuredRoom.description}</p>
+        <div className="flex flex-col md:flex-row md:items-end justify-between mt-5">
+          <div className="flex mb-3 md:mb-0">
+            <div className="flex gap-3 flex-col items-center justify-center mr-4">
+              <p className="text-xs lg:text-xl text-center"> Start From</p>
+              <p className="md:font-bold flex font-medium text-lg xl:text-5xl">
+                ${featuredRoom.price}
+              </p>
+            </div>
+            <div className="flex gap-3 flex-col items-center justify-center mr-4">
+              <p className="text-xs lg:text-xl text-center"> Discount</p>
+              <p className="md:font-bold flex font-medium text-lg xl:text-5xl">
+                ${featuredRoom.discount}
+              </p>
+            </div>
+          </div>
+          <Link
+            href={`/rooms/${featuredRoom.slug.current}`}
+            className="border h-fit text-center border-x-tertiary-dark text-tertiary-dark px-3 py-2 lg:py-5 lg:px-7 rounded-2xl font-bold lg:text-xl"
+          >
+            More Details
+          </Link>
         </div>
       </div>
     </section>
