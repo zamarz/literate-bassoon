d08fe7878f52bda5ff3abe243f05d92620f0eb55
zamarz <zachmarzouk@gmail.com>
2024-01-24 15:04:52 +0000

    Create new Sanity query for individual room

---
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const RoomDetails = (props: { params: { slug: string } }) => {
+  const {
+    params: { slug },
+  } = props;
+
+  return <div>RoomDetails</div>;
+};
+
+export default RoomDetails;
--- src/libs/sanityQueries.ts
+++ src/libs/sanityQueries.ts
@@ -7,3 +7,7 @@ export const getFeaturedRoomQuery = groq`*[_type == 'hotelRoom' && isFeatured ==
 export const getRoomsQuery = groq`*[_type == 'hotelRoom'] {
     _id, coverImage, description, dimension, isBooked, isFeatured, name, price, slug, type,
 }`;
+
+export const getRoom = groq`*[_type == 'hotelRoom' && slug.current == $slug][0] {
+    _id, coverImage, description, dimension, isBooked, discounts, images, isFeatured, name, price, numberOfBeds, offeredAmenities, slug, specialNote, type
+}`;
