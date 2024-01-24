dc131ee210e5ff6aded2c0f725be2f9919d811d2
zamarz <zachmarzouk@gmail.com>
2024-01-24 11:47:23 +0000

    Alter card description

--- src/components/RoomCard/RoomCard.tsx
+++ src/components/RoomCard/RoomCard.tsx
@@ -30,7 +30,7 @@ const RoomCard: FC<Props> = (props) => {
         </div>
         <p className="pt-2 text-xs">{type} Room</p>
 
-        <p className="pt-3 pb-6">{description}</p>
+        <p className="pt-3 pb-6">{description.slice(1, 100)}...</p>
 
         <Link
           className="bg-primary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:-translate-y-2 hover:shadow-lg transition-all duration-500"
