21c7e93c560444112d4247c6b15f191f8857ebc0
zamarz <zachmarzouk@gmail.com>
2024-01-24 09:14:24 +0000

    Create room model with types

---
+++ src/app/models/room.ts
@@ -0,0 +1,36 @@
+type CoverImage = {
+  url: string;
+};
+
+type Image = {
+  key: string;
+  url: string;
+};
+
+type Amenity = {
+  _key: string;
+  amentiy: string;
+  icon: string;
+};
+
+type Slug = {
+  _type: string;
+  current: string;
+};
+
+export type Room = {
+  _id: string;
+  coverImage: CoverImage;
+  description: string;
+  discount: number;
+  images: Image[];
+  isBooked: boolean;
+  isFeatured: boolean;
+  name: string;
+  numberOfBeds: number;
+  offeredAmenities: Amenity[];
+  price: number;
+  slug: Slug;
+  specialNote: string;
+  type: string;
+};
