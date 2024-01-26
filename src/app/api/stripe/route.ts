704401dc8fba3ea49b707bf02119ae015f368b47
zamarz <zachmarzouk@gmail.com>
2024-01-26 14:49:46 +0000

    Add and install stripe

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,83 @@
+import { getRoom } from "@/libs/apis";
+import { authOptions } from "@/libs/auth";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2023-10-16",
+});
+
+type RequestData = {
+  checkinDate: string;
+  checkoutDate: string;
+  adults: number;
+  children: number;
+  numberOfDays: number;
+  hotelRoomSlug: string;
+};
+
+export async function POST(req: Request, res: Response) {
+  const {
+    checkinDate,
+    checkoutDate,
+    adults,
+    children,
+    numberOfDays,
+    hotelRoomSlug,
+  }: RequestData = await req.json();
+
+  if (
+    !checkinDate ||
+    !checkoutDate ||
+    !adults ||
+    !hotelRoomSlug ||
+    !numberOfDays
+  ) {
+    return new NextResponse("Please ensure all fields are complete", {
+      status: 400,
+    });
+  }
+
+  const origin = req.headers.get("origin");
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new NextResponse("Please sign in", {
+      status: 400,
+    });
+  }
+
+  const userId = session.user.id;
+
+  const formattedCheckoutDate = checkoutDate.split("T")[0];
+  const formattedCheckinDate = checkinDate.split("T")[0];
+
+  try {
+    const room = await getRoom(hotelRoomSlug);
+    const discountPrice = room.price - (room.price / 100) * room.discount;
+    const totalPrice = discountPrice * numberOfDays;
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      mode: "payment",
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: room.name,
+              images: room.images.map((image) => image.url),
+            },
+          },
+        },
+      ],
+      payment_method_types: ["card"],
+      success_url: `${origin}/users/${userId}`,
+    });
+  } catch (error: any) {
+    console.log("Payment failed");
+    return new NextResponse(error, { status: 500 });
+  }
+}
