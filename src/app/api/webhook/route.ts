c2ae3a8a85f912320bf7558dec230a63a701a154
zamarz <zachmarzouk@gmail.com>
2024-01-29 11:29:26 +0000

    Link to stripe

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const checkout_session_completed = "checkout.session.completed";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2023-10-16",
+});
+
+export async function POST(req: Request, res: Response) {
+  const reqBody = await req.text();
+  const sig = req.headers.get("stripe-signature");
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
+
+  let event: Stripe.Event;
+
+  try {
+    if (!sig || webhookSecret) return;
+    event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 500 });
+  }
+
+  console.log(event);
+
+  switch (event.type) {
+    case checkout_session_completed:
+      const session = event.data.object;
+      console.log(session);
+      return NextResponse.json("Booking successful", {
+        status: 200,
+        statusText: "Booking successful",
+      });
+      break;
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+      break;
+  }
+
+  return NextResponse.json("Event Received", {
+    status: 200,
+    statusText: "Event Received",
+  });
+}
